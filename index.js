64772e989465c5c4ca8ec3aed3706026d94c23bf
ibnuazis <81544805+bodoamat1902@users.noreply.github.com>
2022-09-27 16:18:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ const menu = require("./lib/menu.js")
 //Setting
 
 const apivhtear = 'Apikey vhtear';
-const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
+const apibarbar = 'Apikey manaketehe';
+const BotName = 'VIP BOT'; 
+const instagram = 'permen1947'; 
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
             + 'FN:Agus\n' // Nama kamu
             + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'TEL;type=CELL;type=VOICE;waid=6281314205935:+62 813-1420-5935\n' //081314205935
             + 'END:VCARD'
 const
 {
